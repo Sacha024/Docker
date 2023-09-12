db8a4e8ef90771a28a19d07afb23fbf97c6c2adb
Sacha024 <131723896+Sacha024@users.noreply.github.com>
2023-09-12 16:13:02 +0200

    yo

--- src/main/java/com/formation/testdocker/IndexController.java
+++ src/main/java/com/formation/testdocker/IndexController.java
@@ -1,6 +1,5 @@
 package com.formation.testdocker;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +15,6 @@ public class IndexController {
     }
 
 
-    @Value("${title}")
-    private String title;
 
     @GetMapping("/")
         public Iterable<ProductEntity> index() {
